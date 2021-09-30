b8929b0cfc4e8f2dc040ae132b147f9a231b75b1
Kenny <kenny@Kennys-MacBook-Air.local>
2021-09-30 10:35:11 -0400

    added strings project solution

--- 01Strings/String/Tester+Strings.swift
+++ 01Strings/String/Tester+Strings.swift
@@ -11,28 +11,34 @@ extension Tester {
 
     /// Using print command
     func printWord(word: String) {
-        
+           print(word)
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
-        
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-        
+        for x in word{
+            print(x)
+        }
     }
     
     /// given a word, flip its characters, and print the result
     /// e.g. football -> llabtoof
     func flipWord(word: String) {
-        
+        print(String(word.reversed()))
     }
     
     /// Using for-in-enumerated(), print odd-position chars in word
     func printOddCharsEnumerated(word: String) {
-        
+        for(n, c) in word.enumerated(){
+            if n%2 == 0 {
+                print(c);
+            }
+        }
     }
     
     /**
@@ -44,11 +50,16 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
-        
+    let fst = word[word.startIndex];
+        if fst == "a" || fst == "e" || fst == "o" || fst == "i" || fst == "u"  {
+            print("\(word)yay")
+        } else{
+            print("\(word.suffix(word.count-1))\(word.prefix(1))ay")
+        }
     }
     
     /// Using loop and string.index, return true if input is a palindrome
     func isPalindrome(word: String) -> Bool {
-        return false
+        return String(word.reversed()) == word
     }
 }
