f463f31bd3222e5e55299a9aef90314e587b6734
Kenny <kenny@Kennys-MacBook-Air.local>
2021-09-30 11:34:05 -0400

    added dictionary project

--- 02Arrays/Array/Tester.swift
+++ 02Arrays/Array/Tester.swift
@@ -6,23 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 class Tester {
     static let shared = Tester()
     
     /// 1. Write a function that takes an array of numbers and returns an array with each of the numbers multiplied by 2
     func multiplyByTwo(numbers original: [Int]) -> [Int] {
-        []
+        var xd = original
+        for i in original.indices {
+            xd[i] *= 2
+        }
+        return xd
     }
 
     /// 2. Write a function to return the last half of the array, not including the median
     func lastHalf(array: [Int]) -> [Int] {
-        []
+        return array.suffix(array.count/2)
+        
     }
 
     /// 3. Write a function that can creates an array populated with integers going from start (input) to end (input)
     func createArray(from start: Int, to end: Int) -> [Int] {
-        []
+        var sol = [Int]()
+        var s = start
+        while s <= end {
+            sol.append(s)
+            s += 1
+        }
+        return sol;
     }
 
     /**
@@ -32,18 +44,42 @@ class Tester {
         [7,8,9]]
      */
     func create2DArray() -> [[Int]] {
-        [[]]
+        var arr = [[Int]]()
+        arr.append([1,2,3])
+        arr.append([4,5,6])
+        arr.append([7,8,9])
+        return arr;
     }
 
     /// 5. Write a function that returns the number of pairs of elements that sums up to 0
     /// Input [1,2,3,-1,-2,-4] -> Output 2
-    func findPairsOfOpposites(nums: [Int]) -> Int {
-        1
+    func findPairs(nums: [Int]) -> Int {
+        var sol = 0;
+        
+        for (i, x) in nums.enumerated() {
+                for y in nums[i+1 ..< nums.count] {
+                    if x + y == 0 {
+                        sol += 1
+                    }
+                }
+            }
+        
+        
+        return sol;
     }
     
     /// 6. Returns an array of indexes where the char occurs in input word
     func positionsOf(character: Character, in word: String) -> [Int] {
-        []
+        var sol = [Int]()
+        
+        for (i, x) in word.enumerated() {
+            if x == character {
+                sol.append(i)
+            }
+        }
+        
+        
+        return sol
     }
     
     /**
@@ -54,16 +90,39 @@ class Tester {
         Ex. [ I, I, O] -> 2
      */
     func minimumChairs(array: [Character]) -> Int {
-        1
+        var sol = 0;
+        var run = 0;
+        
+        for x in array {
+            if x == "I" {
+                run += 1
+                sol = max(sol, run)
+            } else {
+                run -= 1
+            }
+        }
+        
+        return sol;
     }
     
     /// 8. Pig latin but with words separated by spaces
     func pigLatinSentence(sentence: String) -> String {
-        ""
+        var array = sentence.components(separatedBy: " ")
+
+        for (i, _) in array.enumerated() {
+            array[i] = returnPigLatin(word: array[i])
+        }
+        print(array)
+        return array.joined(separator: " ")
     }
     
     func returnPigLatin(word: String) -> String {
-        ""
+        let fst = word[word.startIndex];
+            if fst == "a" || fst == "e" || fst == "o" || fst == "i" || fst == "u"  {
+                return("\(word)yay")
+            } else{
+                return("\(word.suffix(word.count-1))\(word.prefix(1))ay")
+            }
     }
     
     /**
@@ -73,7 +132,17 @@ class Tester {
     
     
     func maxProfit(array: [Int]) -> Int {
-        1
+        var min = Int.max;
+        var sol = 0
+        for (i, _) in array.enumerated() {
+            if array[i] < min{
+                min = array[i]
+            } else if array[i] - min > sol {
+                sol = array[i] - min
+            }
+        }
+        
+        return sol;
     }
     
     /**
@@ -82,6 +151,18 @@ class Tester {
         [1, 4, 8, 4] -> [1, 2, 3, 2]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
-        []
+        var yep = array
+        var c = 1;
+        var dict: [Int: Int] = [:];
+        for (i, x) in array.enumerated() {
+            if dict[x] == nil {
+                dict[x] = c;
+                c += 1;
+            }
+            yep[i] = dict[x, default: 0]
+        }
+        
+        return yep
+        
     }
 }
--- 03SetsDictionary/SetDictionaryProject/Tester.swift
+++ 03SetsDictionary/SetDictionaryProject/Tester.swift
@@ -10,35 +10,77 @@ import Foundation
 struct Tester {
     /// Remove an array with the duplicated values removed
     func removeDups(nums: [Int]) -> [Int] {
-        
-        return []
+        return Array(Set(nums))
     }
     
     /// Find the nums that exist in both arrays
     func findCommonNums(from firstNums: [Int], and secondNums: [Int]) -> [Int] {
+        var fTemp = Set(firstNums)
+        fTemp = fTemp.intersection(secondNums)
         
-        return []
+        return Array(Set(fTemp))
+        
+        
+        
+        
+        
     }
     
     /// first: "hello", second: "bye" -> ["e"]
     func findMatchingLetters(firstWord: String, secondWord: String) -> [Character] {
-        return []
+        var fWord = Array(firstWord)
+        return Array(Set(fWord).intersection(Set(secondWord)))
+        
     }
     
     
     /// Create a dictionary of the counts of the letters, ignoring spaces
     func createCounter(string: String) -> [Character: Int] {
-        return [:]
+        var yep: [Character: Int] = [:]
+        
+        for x in string {
+            if x != " " {
+                yep[x, default: 0] += 1;
+            }
+        }
+        return yep
     }
     
     /// Find most frequent letter in string: "hello there" -> "e"
     func mostFrequentLetter(string: String) -> Character {
-        return "."
+        let ctr = createCounter(string: string)
+        
+        var max = 0;
+        var sol: Character = " "
+        
+        for x in string {
+            if ctr[x, default: -1] > max {
+                max = ctr[x, default: -1]
+                sol = x
+            }
+        }
+        
+        return sol
     }
     
     /// O(n) time: avoid nested for loops
-    func findPairs(nums: [Int]) -> Int {        
-        return 1
+    func findPairs(nums: [Int]) -> Int {
+        var sol = 0
+        var yep: [Int: Int] = [:]
+        
+        for x in nums {
+            yep[x, default: 0] += 1;
+            
+        }
+        
+        for x in nums {
+            let a = yep[x, default: 0]
+            let b = yep[x * -1, default: 0]
+            sol += min(a, b)
+            yep[x, default: 0] = 0
+            yep[x * -1, default: 0] = 0
+        }
+        return sol
     }
     
     /**
@@ -48,7 +90,23 @@ struct Tester {
      [1, 4, 8, 1] -> [1, 2, 3, 1]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
-        return [1]
+        var yep = array
+        var c: Int = 1;
+        
+        var dict : [Int: Int] = [:]
+        
+        for x in yep.sorted() {
+            if dict[x, default: 0] == 0 {
+                dict[x] = c;
+                c += 1
+            }
+        }
+        
+        for (i, _) in yep.enumerated() {
+            yep[i] = dict[yep[i], default: 0]
+        }
+        
+        return yep
     }
     
 }
